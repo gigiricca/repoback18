1431a3addbb8954033ffb74a2e31846f6e08899c
Gisselle Ricca <gissellericca@gmail.com>
2024-09-17 23:55:34 -0300

    Update pass config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "G1sse.2806#",
+    password: "G1sse.2806##",
     database: "cateringlink",
     host: "127.0.0.1",
     dialect: "mysql",
